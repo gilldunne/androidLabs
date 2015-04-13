399505496f21c0890a860f01536eb8296a24cc23
gilldunne <x00094469@ittd.ie>
2015-04-13 09:54:34 +0100

    Radio Button added

---
+++ RadioButton/app/src/main/java/com/example/gillian/radiobutton/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.gillian.radiobutton;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onRadioButtonClicked(View view){
+        boolean checked = ((RadioButton)view).isChecked();
+        String message = "Radio : ";
+
+        switch(view.getId()) {
+            case R.id.radioButton1:
+                if (checked) {
+                    message += "One";
+                }
+                break;
+            case R.id.radioButton2:
+                if (checked) {
+                    message += "Two";
+                }
+                break;
+            default:
+                break;
+        }
+        showMessage(message);
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
